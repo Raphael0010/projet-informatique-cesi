3a84000748398bf8d2170f75bbf2f504d02f453f
Raphael Mortier <raphaelmortier@outlook.Fr>
2019-07-16 11:05:12 +0200

    Work In Progress 🚀

--- src/components/Supervision/Supervision.tsx
+++ src/components/Supervision/Supervision.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Icon } from "antd";
 import "./Supervision.css";
 
 const Supervision: React.FC = () => {
-  return <>Supervision</>;
+  return (
+    <>
+      <div className="title-super-vision">
+        <h3>
+          <Icon className="logo-title-super-vision" type="unordered-list" />
+          Liste des équipements connectés
+        </h3>
+      </div>
+    </>
+  );
 };
 
 export default Supervision;
