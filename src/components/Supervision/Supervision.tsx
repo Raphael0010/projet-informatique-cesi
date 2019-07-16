94f7ecaefc1adb7b118b9b3f834874a45037d5bf
Raphael Mortier <raphaelmortier@outlook.Fr>
2019-07-16 16:26:34 +0200

    Work In Progress 🚀

--- src/components/Supervision/Supervision.tsx
+++ src/components/Supervision/Supervision.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Icon } from "antd";
+import socketIOClient from "socket.io-client";
 import "./Supervision.css";
 
+const url = "http://127.0.0.1:3030";
+
 const Supervision: React.FC = () => {
+  const getIp = () => {
+    const socket = socketIOClient(url);
+    socket.on("cmd return", (c: any) => {
+      console.log(c);
+    });
+    socket.on("snmp_get_ip_return", (c: any) => {
+      console.log(c);
+    });
+  };
+
+  useEffect(() => {
+    getIp();
+  }, []);
+
   return (
     <>
       <div className="title-super-vision">
